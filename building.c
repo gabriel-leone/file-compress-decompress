c5e3c3d444bc65cdb4da169a65f5b1356277daf0
Gabriel Leone <leonepgabriel@gmail.com>
2023-12-19 16:51:28 -0300

    Ajeitando alguns detalhes

--- building.c
+++ building.c
@@ -1,88 +1,93 @@
 #include "building.h"
 
-
-Tree *func_exception(FILE *arquivo){
+Tree *func_exception(FILE *arquivo)
+{
     BYTE character;
     fscanf(arquivo, "%c", &character);
 
-    Tree *h_tree = creat_node(character,0);
+    Tree *h_tree = creat_node(character, 0);
     fscanf(arquivo, "%c", &character);
-    if(character == '\\') fscanf(arquivo, "%c", &character);
+    if (character == '\\')
+        fscanf(arquivo, "%c", &character);
     h_tree->left = creat_node(character, 0);
-    return (Tree*) h_tree;
-
+    return (Tree *)h_tree;
 }
-Tree* rebuild_huffman_tree (FILE *arquivo){
+Tree *rebuild_huffman_tree(FILE *arquivo)
+{
 
     int condition = 1;
     BYTE character;
 
     fscanf(arquivo, "%c", &character);
 
-    if(character != '*')
+    if (character != '*')
     {
         condition = 0;
     }
-    if(character == '\\')
+    if (character == '\\')
     {
         fscanf(arquivo, "%c", &character);
         condition = 0;
     }
-    Tree *h_tree = creat_node(character,0);
-    if(condition)
+    Tree *h_tree = creat_node(character, 0);
+    if (condition)
     {
         h_tree->left = rebuild_huffman_tree(arquivo);
         h_tree->right = rebuild_huffman_tree(arquivo);
     }
 
-	return (Tree*)h_tree;
+    return (Tree *)h_tree;
 }
 
-Tree* build_huffman_tree(int *array){
-    
+Tree *build_huffman_tree(int *array)
+{
+
     priority_queue *pq = creat_priority_queue();
     Tree *huffman_tree;
 
     int i;
 
-    for(i = 255; i >= 0; i--){
-        if(*(array+i) != 0)
-            enqueue(creat_node(i, *(array+i)), pq);
+    for (i = 255; i >= 0; i--)
+    {
+        if (*(array + i) != 0)
+            enqueue(creat_node(i, *(array + i)), pq);
     }
 
     huffman_tree = build_Tree(pq);
 
-    return  (Tree*)huffman_tree;
+    return (Tree *)huffman_tree;
 }
 
-Tree* build_Tree(priority_queue *pq){
+Tree *build_Tree(priority_queue *pq)
+{
 
     Tree *aux;
 
-    while(1){
+    while (1)
+    {
 
         aux = creat_node('*', 0);
         aux->left = dequeue(pq);
-        
-        if(aux->left != NULL)
+
+        if (aux->left != NULL)
             aux->frequency += aux->left->frequency;
-        
+
         aux->right = dequeue(pq);
-        if(aux->right != NULL)
+        if (aux->right != NULL)
             aux->frequency += aux->right->frequency;
-        
-        if(pq->head == NULL)
+
+        if (pq->head == NULL)
             break;
-        
+
         enqueue(aux, pq);
-        
     }
     return aux;
 }
 
-int creating_huffman_string(Tree *huffman, FILE *header){
-  
-    int *size = (int*) malloc(sizeof(int));
+int creating_huffman_string(Tree *huffman, FILE *header)
+{
+
+    int *size = (int *)malloc(sizeof(int));
     BYTE aux = 0;
     (*size) = 0;
     fprintf(header, "%c", aux);
--- building.h
+++ building.h
@@ -3,25 +3,10 @@
 
 Tree *func_exception(FILE *arquivo);
 
-/*
- * Constrói a árvore de huffman a partir do cabeçalho do arquivo.
- * Recebe o ponteiro do arquivo e retorna um ponteiro para a árvore.
- */
 Tree* rebuild_huffman_tree (FILE *arquivo);
 
-/*
- * Recebe um ponteiro pra inteiro, correspondendo ao array de frequência e retorna a arvore montada.
- */
 Tree* build_huffman_tree(int *array);
 
-/*
- * Recebe uma fila e monta a árvore de huffman. Retorna a árvore.
- */
 Tree *build_Tree(priority_queue *pq);
 
-/*
- * Recebe um ponteiro para a árvore de huffman e inicia o processo que gera a partir dela uma string em pré-ordem. 
- * Recebe um ponteiro para o arquivo de saída e escreve dois bytes vazios no início.
- * Retorna o tamaho da árvore.
- */
 int creating_huffman_string(Tree *huffman, FILE *header);
