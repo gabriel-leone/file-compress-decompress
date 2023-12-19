1275f4d884076d594bcbc0ed471d124978d137ef
Gabriel Leone <leonepgabriel@gmail.com>
2023-12-19 17:27:51 -0300

    Comentários dos arquivos compress.c e decompress.c

--- compress.c
+++ compress.c
@@ -1,97 +1,119 @@
 #include "compress.h"
 
+// Função para contar a frequência de cada caractere no arquivo
 void frequency_count(FILE *arquivo, int *array)
 {
 
     BYTE character;
 
+    // Lê cada caractere do arquivo até o final
     while ((fscanf(arquivo, "%c", &character)) != EOF)
     {
-        ++array[character];
+        ++array[character]; // Incrementa a contagem para o caractere lido
     }
 
     return;
 }
 
+// Função para adicionar um bit à posição especificada em um byte
 BYTE add_bit(BYTE c_saida, short int pos)
 {
+    // Retorna o byte com o bit adicionado na posição especificada
     return (c_saida | (1 << (7 - pos)));
 }
 
+// Função para escrever o arquivo comprimido
 void write_compress(BYTE tabela[][MAX_TABLE_SIZE], FILE *arquivo, FILE *saida, int tree_size)
 {
 
     BYTE aux, character = 0, cond = 1;
     short int size = 0, position = 0;
 
+    // Lê cada caractere do arquivo até o final
     while ((fscanf(arquivo, "%c", &aux)) != EOF)
     {
         position = 0;
 
+        // Percorre a tabela de compressão para o caractere lido
         while (tabela[aux][position] != '\0')
         {
+            // Se o tamanho é 8, escreve o caractere no arquivo de saída e reinicia o caractere e o tamanho
             if (size == 8)
             {
                 fprintf(saida, "%c", character);
                 size = 0;
                 character = 0;
             }
+            // Se o bit na posição atual é 1, adiciona o bit ao caractere
             if (tabela[aux][position] & 1)
                 character = add_bit(character, size);
+            // Incrementa o tamanho e a posição
             ++size;
             ++position;
         }
     }
 
+    // Calcula o número de bits de lixo no último byte
     BYTE lixo = (8 - size) << 5;
     fprintf(saida, "%c", character);
 
-    // SETANDO O PRIMEIRO BYTE (BITS DO LIXO)
-
+    // Posiciona o ponteiro do arquivo no início
     fseek(saida, 0, SEEK_SET);
+    // Combina os bits de lixo com os 3 bits mais significativos do tamanho da árvore
     lixo = lixo | tree_size >> 8;
+    // Escreve o primeiro byte no arquivo de saída
     fprintf(saida, "%c", lixo);
+    // Obtém os 8 bits menos significativos do tamanho da árvore
     lixo = tree_size & 255;
+    // Escreve o segundo byte no arquivo de saída
     fprintf(saida, "%c", lixo);
 
     return;
 }
 
+// Função para criar a tabela de compressão
 void creat_table(BYTE tabela[][MAX_TABLE_SIZE], Tree *bt, BYTE *string, int position)
 {
 
+    // Verifica se o nó atual é uma folha (não tem filhos)
     if (bt->left == NULL && bt->right == NULL)
     {
-        string[position] = '\0';
-        strncpy(tabela[bt->character], string, position + 1);
+        string[position] = '\0';                              // Termina a string com um caractere nulo
+        strncpy(tabela[bt->character], string, position + 1); // Copia a string para a tabela de compressão na posição do caractere do nó
         return;
     }
     if (bt->left != NULL)
     {
+        // Se o nó atual tem um filho à esquerda, adiciona '0' à string
         string[position] = '0';
+        // Chama a função recursivamente para o filho à esquerda
         creat_table(tabela, bt->left, string, position + 1);
     }
     if (bt->right != NULL)
     {
+        // Se o nó atual tem um filho à direita, adiciona '1' à string
         string[position] = '1';
+        // Chama a função recursivamente para o filho à direita
         creat_table(tabela, bt->right, string, position + 1);
     }
 }
 
+// Função para comprimir o arquivo
 void compress(char *nome_entrada)
 {
 
     char nome_saida[MAX_FILENAME_SIZE], aux[MAX_TABLE_SIZE];
     int frequency[MAX_FREQUENCY_SIZE];
-    memset(frequency, 0, sizeof(frequency));
+    memset(frequency, 0, sizeof(frequency)); // Inicializa o array de frequência com zeros
 
     FILE *arquivo;
     FILE *novo_arquivo;
 
+    // Loop infinito para tentar abrir o arquivo de entrada
     while (1)
     {
-        system("cls");
-        arquivo = fopen(nome_entrada, "rb");
+        system("cls");                       // Limpa a tela do terminal
+        arquivo = fopen(nome_entrada, "rb"); // Tenta abrir o arquivo de entrada no modo de leitura binária
         if (arquivo == NULL)
         {
             printf("O nome do arquivo ou caminho esta incorreto!\n Tente novamente...");
@@ -105,29 +127,29 @@ void compress(char *nome_entrada)
     strcat(nome_saida, ".comp");
 
     frequency_count(arquivo, frequency);
-    fseek(arquivo, 0, SEEK_SET);
-    system("cls");
-    printf("Aguarde ...\n");
+    fseek(arquivo, 0, SEEK_SET); // Reposiciona o ponteiro do arquivo de entrada para o início
 
+    // Cria a árvore de Huffman a partir do array de frequências
     Tree *bt = build_huffman_tree(frequency);
-    system("cls");
-    printf("Aguarde ...\n");
 
+    // Cria um array bidimensional para armazenar a tabela de compressão
     BYTE tabela[256][150];
     creat_table(tabela, bt, aux, 0);
 
     system("cls");
-    printf("Aguarde ...\n");
 
+    // Tenta abrir o novo arquivo no modo de escrita binária
     novo_arquivo = fopen(nome_saida, "wb");
 
     int size_tree;
     BYTE aux1;
+    // Chama a função para criar a string de Huffman e armazena o tamanho da árvore
     size_tree = creating_huffman_string(bt, novo_arquivo);
 
+    // Chama a função para escrever o arquivo comprimido
     write_compress(tabela, arquivo, novo_arquivo, size_tree);
 
-    printf("Concluido com exito!");
+    printf("Concluido com exito!\n\n");
 
     fclose(arquivo);
     fclose(novo_arquivo);
--- decompress.c
+++ decompress.c
@@ -1,49 +1,62 @@
 #include "decompress.h"
 
+// Função para escrever o arquivo descomprimido
 void write_decompress(Tree *bt, FILE *entrada, FILE *saida, int lixo)
 {
+    // Define um ponteiro auxiliar para a árvore de Huffman
     Tree *aux = bt;
+    // Define variáveis para armazenar os caracteres lidos do arquivo e um auxiliar
     BYTE character, aux1;
     int i;
 
+    // Lê o primeiro caractere do arquivo de entrada
     fscanf(entrada, "%c", &character);
     aux1 = character;
 
+    // Lê cada caractere do arquivo de entrada até o final
     while (fscanf(entrada, "%c", &character) != EOF)
     {
-
+        // Loop para cada bit do caractere
         for (i = 7; i >= 0; i--)
         {
-
+            // Se o nó atual é uma folha (não tem filhos)
             if (aux->left == NULL && aux->right == NULL)
             {
+                // Escreve o caractere do nó no arquivo de saída
                 fprintf(saida, "%c", aux->character);
+                // Reinicia o ponteiro auxiliar para o início da árvore
                 aux = bt;
             }
-
+            // Se o bit na posição atual é 1, move o ponteiro auxiliar para a direita
             if (aux1 & 1 << i)
                 aux = aux->right;
+            // Se o bit na posição atual é 0, move o ponteiro auxiliar para a esquerda
             else
                 aux = aux->left;
         }
         aux1 = character;
     }
 
+    // Loop para os bits restantes no último byte
     for (i = 7; lixo <= 8; lixo++, i--)
     {
+        // Se o nó atual é uma folha (não tem filhos)
         if (aux->left == NULL && aux->right == NULL)
         {
             fprintf(saida, "%c", aux->character);
             aux = bt;
         }
+        // Se o bit na posição atual é 1, move o ponteiro auxiliar para a direita
         if (aux1 & 1 << i)
             aux = aux->right;
+        // Se o bit na posição atual é 0, move o ponteiro auxiliar para a esquerda
         else
             aux = aux->left;
     }
     return;
 }
 
+// Função para verificar se o nome do arquivo termina com ".comp"
 int verification(char *nome)
 {
     int condition = 0, i, j, size = strlen(nome);
@@ -62,6 +75,7 @@ int verification(char *nome)
     return condition;
 }
 
+// Função para descomprimir o arquivo
 void decompress(char *nome_entrada)
 {
 
@@ -74,10 +88,11 @@ void decompress(char *nome_entrada)
     int lixo, size_tree = 0, i;
     Tree *bt = NULL;
 
+    // Loop infinito para tentar abrir o arquivo de entrada
     while (1)
     {
-
         system("cls");
+        // Tenta abrir o arquivo de entrada no modo de leitura binária
         arquivo_entrada = fopen(nome_entrada, "rb");
 
         if (arquivo_entrada == NULL)
@@ -86,35 +101,40 @@ void decompress(char *nome_entrada)
             system("pause");
             continue;
         }
+        // Se a verificação do nome do arquivo passar (ou seja, o arquivo tem a extensão ".comp")
         if (verification(nome_entrada))
             continue;
         break;
     }
 
+    // Copia o nome do arquivo de entrada para o nome do arquivo de saída, excluindo a extensão ".comp"
     strncpy(nome_saida, nome_entrada, strlen(nome_entrada) - 5);
 
-    system("cls");
-    printf("Aguarde ...\n");
-
-    // LENDO O TAMANHO DO LIXO
+    // Lê o primeiro caractere do arquivo de entrada
     fscanf(arquivo_entrada, "%c", &character);
+    // Extrai os 3 bits mais significativos, que representam o tamanho do lixo
     lixo = character >> 5;
 
-    // LENDO O TAMANHO DA ARVORE
+    // Extrai os 5 bits menos significativos do primeiro caractere, que representam a parte alta do tamanho da árvore
     size_tree = character & 0b00011111;
+    // Desloca os bits para a esquerda para fazer espaço para a parte baixa do tamanho da árvore
     size_tree = size_tree << 8;
+    // Lê o próximo caractere do arquivo de entrada
     fscanf(arquivo_entrada, "%c", &character);
+    // Combina a parte baixa do tamanho da árvore com a parte alta
     size_tree = size_tree | character;
 
     system("cls");
-    printf("Aguarde ...\n");
 
+    // Se o tamanho da árvore é 2, chama a função de exceção para reconstruir a árvore de Huffman
     if (size_tree == 2)
         bt = (Tree *)func_exception(arquivo_entrada);
+    // Se o tamanho da árvore não é 2, chama a função normal para reconstruir a árvore de Huffman
     else
         bt = rebuild_huffman_tree(arquivo_entrada);
     arquivo_saida = fopen(nome_saida, "wb");
 
+    // Chama a função para escrever o arquivo descomprimido
     write_decompress(bt, arquivo_entrada, arquivo_saida, lixo);
 
     printf(" Concluido com exito\n");
